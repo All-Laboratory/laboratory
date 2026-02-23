8074029822e925f33640e5a2951572a656a45497
MOHAMMED MUZZAMMIL A K <mohammedmuzzammil.offic@gmail.com>
2026-02-23 20:27:26 +0530

    Add hc.java file for Hill Cipher implementation

--- hc.java
+++ hc.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ImplementationOfHillCipher {
+public class hc {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
