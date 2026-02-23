c5946ef6241f469828de83a124071d3282f75713
MOHAMMED MUZZAMMIL A K <mohammedmuzzammil.offic@gmail.com>
2026-02-23 20:26:31 +0530

    Add cc.java for Caesar Cipher implementation

--- cc.java
+++ cc.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ImplementationOfCaesarCipher {
+public class CC {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
