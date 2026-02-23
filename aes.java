339ab1717ea3746a75ac4ef5dd818600e8d09f48
MOHAMMED MUZZAMMIL A K <mohammedmuzzammil.offic@gmail.com>
2026-02-23 20:31:04 +0530

    Update and rename AES.java to aes.java

--- aes.java
+++ aes.java
@@ -4,7 +4,7 @@ import javax.crypto.SecretKey;
 import java.util.Base64;
 import java.util.Scanner;
 
-public class AES {
+public class aes {
 
     public static void main(String[] args) throws Exception {
 
